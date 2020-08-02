59af8979df340489af725c2b50f67abafbaa3543
salama <hooha2@outlook.com>
2020-08-02 15:00:33 +0200

    new change

--- test.py
+++ test.py
@@ -2,8 +2,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as  np
 df = pd.DataFrame({"a":[10,15,20,25,30,35,40,45]})
 
-print(df)
-sns.displot(df)
+m = np.mean(df.a)
+print('This Distribution of Pupalution:' ,m)
+
+
+sns.distplot(df.a)
 plt.show()
+
+
+for i in df.a:
+    if i >=30:
+        print('this number more then 30kr : {}'.format(i))
+
+
